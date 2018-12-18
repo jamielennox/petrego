fa4e6ec6ebff8430d725fe7467ecdf87eeaac3ed
Khoa Vuong <khoa.vuong@auspost.com.au>
2018-12-18 17:22:18 +1100

    Initial code commit with Spring scaffolding.

---
+++ src/main/java/com/petrego/petrego/PetregoApplication.java
@@ -0,0 +1,14 @@
+package com.petrego.petrego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetregoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetregoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/petrego/petrego/PetregoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.petrego.petrego;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetregoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
