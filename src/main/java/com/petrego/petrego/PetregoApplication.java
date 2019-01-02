421d6d3351905e8d4e9192716f9f341c8dce45b3
Khoa Vuong <khoa.vuong@auspost.com.au>
2019-01-02 15:41:08 +1100

    Add checkstyle plugin.

--- src/main/java/com/petrego/petrego/PetregoApplication.java
+++ src/main/java/com/petrego/petrego/PetregoApplication.java
@@ -2,13 +2,15 @@ package com.petrego.petrego;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class PetregoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PetregoApplication.class, args);
-	}
-
+@EnableJpaRepositories
+@EntityScan
+public class PetregoApplication extends SpringBootServletInitializer {
+    public static void main(final String[] args) {
+        SpringApplication.run(PetregoApplication.class, args);
+    }
 }
-
